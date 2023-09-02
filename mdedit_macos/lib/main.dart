b5eaf540d2149161b9ccfb2e38465d2c9804f627
Orfeo <orfeo18@gmail.com>
2023-09-02 18:04:24 +0100

    Maybe fxed on macosx

--- mdedit_macos/lib/main.dart
+++ mdedit_macos/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:macos_ui/macos_ui.dart';
 import 'package:mdedit_api/init_api.dart';
-import 'package:mdedit_macos/app_macos.dart';
+import 'app_macos.dart';
 import 'package:mdedit_macos/router/router_macos.dart';
 
 void main() async {
